14ee5a73e8f8db7e1a96db35abd052996b9008f9
geroalm <49346206+geroalm@users.noreply.github.com>
2023-11-11 20:49:21 -0300

    Delete Clase 24 - desafio directory

--- Clase 24 - desafio/src/dao/memory/cartsManagerMemory.js
+++
@@ -1,9 +0,0 @@
-export class CartsManagerMemory{
-    constructor(){
-        this.carts = [];
-    };
-
-    async getCarts(){};
-    async createCart(cart){};
-    async addProduct(cartId, productId){};
-};
